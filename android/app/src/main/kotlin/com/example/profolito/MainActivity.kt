083088fa9856507a541d1e523cce673c3391fb7e
Mamin <pg.mohammadamin@gmail.com>
2023-08-04 18:14:13 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/profolito/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.profolito
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext contex) {
+    return MaterialApp();
+  }
+}
