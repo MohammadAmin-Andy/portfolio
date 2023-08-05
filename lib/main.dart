9bbeeb376be34a844b7f9598c98446500abcb04a
Mamin <pg.mohammadamin@gmail.com>
2023-08-05 23:45:01 +0330

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(Application());
@@ -8,6 +9,29 @@ class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext contex) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Soraya'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: _getAppBar(),
+        //body: ,
+      ),
+    );
+  }
+
+  PreferredSizeWidget _getAppBar() {
+    return AppBar(
+      backgroundColor: Color.fromARGB(255, 29, 81, 14),
+      elevation: 10,
+      centerTitle: true,
+      title: Text(
+        'درباره‌ی محمدامین امیرکلائی اندی',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 19.0,
+            color: Color.fromARGB(255, 255, 255, 255)),
+      ),
+    );
   }
 }
